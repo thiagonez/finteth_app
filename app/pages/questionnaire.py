a8049fde223b5d386213d05cfadbaeb0bcf01a56
Thiago <thiagorxsplanilhas@gmail.com>
2025-05-23 15:38:50 -0300

    Update questionnaire.py

--- app/pages/questionnaire.py
+++ app/pages/questionnaire.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from app.components.navigation import navigation_buttons
+from components.navigation import navigation_buttons
 
 def show():
     """Página de questionário comportamental"""
