c690f1fbf2e396d713a2eefd8352ba2f468b5ff6
Thiago <thiagorxsplanilhas@gmail.com>
2025-05-23 15:39:47 -0300

    Update reports.py

--- app/pages/reports.py
+++ app/pages/reports.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
-from app.components.navigation import navigation_buttons
+from components.navigation import navigation_buttons
 
 def show():
     """Página de relatórios detalhados"""
